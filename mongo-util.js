cd7e2afa9bf1ea73bceeb1d0393860923b81e3cb
Adam.Klis <adam.klis@asseco.pl>
2021-01-13 10:49:28 +0100

    docker compose db

--- mongo-util.js
+++ mongo-util.js
@@ -1,5 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://localhost:27017";
+const HOST = process.env.DB_HOST || "localhost";
+const PORT = process.env.DB_PORT || "27017";
+const url =  `mongodb://${HOST}:${PORT}`;
 
 var _db;
 
