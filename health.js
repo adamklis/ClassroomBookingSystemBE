88fd7c1a59b0ce830f079e9ea44cca1d152f8bfc
Adam.Klis <adam.klis@asseco.pl>
2021-01-23 20:02:36 +0100

    healthcheck fix

--- health.js
+++ health.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
   
 router.get('/', function (req, res) {
-    res.send('I\'m alive!');
+    res.send({message: 'I\'m alive!'});
 })
 
 module.exports = router
