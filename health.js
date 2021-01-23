b96ad24b02f4892a23a0115197437039f975e49b
adamklis <klis.adam.0807@gmail.com>
2021-01-23 19:41:34 +0100

    Revert "health ping message service"

--- health.js
+++ health.js
@@ -1,14 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const http = require('http')
   
 router.get('/', function (req, res) {
-    res.send({message: "I\'m alive!"});
-    setTimeout(() => {
-        http.get(req.headers.origin, (response) => {
-            console.log(`frontend status: ${response.statusCode}`);
-        })
-    }, 60000);
+    res.send('I\'m alive!');
 })
 
 module.exports = router
