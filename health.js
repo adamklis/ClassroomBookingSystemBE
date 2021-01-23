e844d0a7348ba60049e2d588327d2d67b432c488
Adam.Klis <adam.klis@asseco.pl>
2021-01-23 13:52:23 +0100

    health ping message service

--- health.js
+++ health.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const http = require('http')
   
 router.get('/', function (req, res) {
-    res.send('I\'m alive!');
+    res.send({message: "I\'m alive!"});
+    setTimeout(() => {
+        http.get(req.headers.origin, (response) => {
+            console.log(`frontend status: ${response.statusCode}`);
+        })
+    }, 60000);
 })
 
 module.exports = router
